4e8881ccbde80709a2d5d6d2c01c7a017684a911
ashiq-a-r <ashiqkallara.cet@gmail.com>
2026-09-14 22:08:22 +0530

    changed channel labels

--- lstmRawTraining.py
+++ lstmRawTraining.py
@@ -99,12 +99,12 @@ NUM_CLASSES = len(CLASS_NAMES)
 # ============================================================
 
 CHANNELS = [
-    "acc_x",
-    "acc_y",
-    "acc_z",
-    "gyro_x",
-    "gyro_y",
-    "gyro_z",
+    "ax",
+    "ay",
+    "az",
+    "gx",
+    "gy",
+    "gz",
     "acc_magnitude",
     "gyro_magnitude",
     "acc_magnitude_change",
